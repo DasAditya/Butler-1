817b1a6e697152259eed6cabf6997a0f3fafbc53
sanketnavin <sanket.navin@gmail.com>
2016-09-19 23:42:23 +0530

    added login, logout, register and their templates

--- home/views.py
+++ home/views.py
@@ -1,6 +1,49 @@
 from django.shortcuts import render
+from .forms import UserForm
+from django.contrib.auth import login, logout, authenticate
 
 
 def index(request):
-    return render(request, 'home/index.html')
+    if request.user.is_authenticated():
+        base = "home/base_logged_in.html"
+    else:
+        base = "home/base_visitor.html"
+    context = {'base_template': base}
+    return render(request, 'home/index.html', context)
 
+
+def register(request):
+    form = UserForm(request.POST or None)
+    if form.is_valid():
+        user = form.save(commit=False)
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        user.set_password(password)
+        user.save()
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return render(request, 'home/index.html', {'base_template': "home/base_logged_in.html"})
+    return render(request, 'home/register.html', {'form': form})
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return render(request, 'home/index.html', {'base_template': "home/base_logged_in.html"})
+    return render(request, 'home/login.html')
+
+
+def logout_user(request):
+    logout(request)
+    form = UserForm(request.POST or None)
+    context = {
+        "form": form,
+    }
+    return render(request, 'home/login.html', context)
--- houseonrent/views.py
+++ houseonrent/views.py
@@ -5,10 +5,18 @@ from .models import House
 
 def index(request):
     all_houses = House.objects.all()
-    context = {'houses': all_houses}
+    if request.user.is_authenticated():
+        base = "home/base_logged_in.html"
+    else:
+        base = "home/base_visitor.html"
+    context = {}
+    context = {'houses': all_houses,
+               'base_template': base,}
     return render(request, 'houseonrent/index.html', context)
+
 
 def detail(request, pk):
     house = House.objects.get(pk=pk)
     context = {'house': house}
     return render(request, 'houseonrent/detail.html', context)
+
